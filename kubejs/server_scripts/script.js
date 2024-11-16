f2448cc3b3e0227316b5c25e99d88b5b5a6735cf
BuildTools <goldenflamezero@gmail.com>
2024-11-16 11:01:52 +1100

    Multiple tips and a few balance changes.

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -934,8 +934,6 @@ ServerEvents.recipes(event => {
 	// #region Progression pacing changes.
 	// nether first.
 	// you should be able to get there without mods, but it'll be longer and less worth it.
-	event.replaceInput({id: 'minecraft:bucket'}, 'minecraft:iron_ingot', '#forge:plates/iron')
-
 	event.remove({id: 'minecraft:flint_and_steel'})
 	// having it pre-damaged seems neat
 	event.shapeless(Item.of('minecraft:flint_and_steel', "{Damage:32,RepairCost:0,display:{Name:'{\"text\":\"Flint and Iron\"}'}}"), ['#forge:rods/iron', 'minecraft:flint'])
